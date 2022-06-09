b6d81074258efdf83566bee540216fdb08808c94
Wallyson <wallyson@aluno.fapce.edu.br>
2022-06-09 07:18:17 -0300

    refactor: alterando sysout

--- src/br/com/dio/calculadora/Calculadora.java
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -7,17 +7,19 @@ public class Calculadora {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Digite o primeiro valor: ");
-	 	int a = scan.nextInt();
+		int a = scan.nextInt();
 		System.out.println("Digite o segundo valor: ");
 		int b = scan.nextInt();
-		
-		int soma = soma(a,b);
+
+		int soma = soma(a, b);
 		int subtracao = subtracao(a, b);
-		int multiplicacao = multiplicacao(a,b);
-		System.out.println(soma);
-		System.out.println(subtracao);
-		System.out.println(multiplicacao);
-		
+		int multiplicacao = multiplicacao(a, b);
+		int divisao = divisao(a, b);
+		System.out.println("soma =" + soma);
+		System.out.println("subtracao =" + subtracao);
+		System.out.println("multiplicacao" + multiplicacao);
+		System.out.println("divisao" + divisao);
+
 	}
 
 	public static int soma(int a, int b) {
@@ -27,8 +29,12 @@ public class Calculadora {
 	public static int subtracao(int a, int b) {
 		return a - b;
 	}
-	
+
 	public static int multiplicacao(int a, int b) {
 		return a * b;
+	}
+
+	public static int divisao(int a, int b) {
+		return a / b;
 	}
 }
