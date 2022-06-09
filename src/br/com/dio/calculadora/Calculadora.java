6434d1ebc90f33d6de7dcf79f4db03db205851d2
Wallyson <wallyson@aluno.fapce.edu.br>
2022-06-09 07:12:04 -0300

    feature: adicionando multiplicacao

--- src/br/com/dio/calculadora/Calculadora.java
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -6,6 +6,18 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Digite o primeiro valor: ");
+	 	int a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		int b = scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a, b);
+		int multiplicacao = multiplicacao(a,b);
+		System.out.println(soma);
+		System.out.println(subtracao);
+		System.out.println(multiplicacao);
+		
 	}
 
 	public static int soma(int a, int b) {
@@ -14,5 +26,9 @@ public class Calculadora {
 
 	public static int subtracao(int a, int b) {
 		return a - b;
+	}
+	
+	public static int multiplicacao(int a, int b) {
+		return a * b;
 	}
 }
