8478f2f2f0b235c5c493b363c00917303879de25
Wallyson <wallyson@aluno.fapce.edu.br>
2022-06-09 07:00:23 -0300

    feature: adicionando scanner

--- src/br/com/dio/calculadora/Calculadora.java
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -1,9 +1,11 @@
 package br.com.dio.calculadora;
 
+import java.util.Scanner;
+
 public class Calculadora {
 
 	public static void main(String[] args) {
-
+		Scanner scan = new Scanner(System.in);
 	}
 
 	public static int soma(int a, int b) {
